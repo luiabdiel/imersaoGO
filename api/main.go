30d7442583e8322d495246699af36ed18b761150
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-01 08:03:30 -0300

    users list API

---
+++ api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/users", getUsers)
+	fmt.Println("api is on in :8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content=Type", "application/json")
+	json.NewEncoder(w).Encode([]User{
+		{
+			ID:   1,
+			Name: "Ronaldo",
+		},
+		{
+			ID:   1,
+			Name: "Messi",
+		},
+	})
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/luiabdiel/imersaoGO
-
-go 1.20
